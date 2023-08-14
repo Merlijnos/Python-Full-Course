cf30ceba87126faa5f1dd4e1fa32572dbbd6a200
Merlijnos <99071140@mydavinci.nl>
2023-08-14 19:50:56 +0200

    Course

---
+++ modules.py
@@ -0,0 +1,13 @@
+# import modules_messages as msg
+from modules_messages import hello, bye
+
+# module = a file containing a set of functions to include in your application
+# used with modular programming, which is the process of breaking a large, unwieldy programming task into separate, smaller, more manageable subtasks or modules
+
+# msg.hello()
+# msg.bye()
+
+hello()
+bye()
+
+help("modules")
