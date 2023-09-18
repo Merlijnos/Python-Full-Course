baa022819e75845c615b944206800b2dca4924eb
Merlijnos <99071140@mydavinci.nl>
2023-09-18 16:32:18 +0200

    python full course

---
+++ higher order functions.py
@@ -0,0 +1,14 @@
+# Higher order functions are functions that take other functions as arguments or return functions as their results.
+
+def loud(text):
+    return text.upper()
+
+def quiet(text):
+    return text.lower()
+
+def hello(func):
+    text = func("Hello")
+    print(text)
+
+hello(loud)
+hello(quiet)
---
+++ lambda.py
@@ -0,0 +1,12 @@
+# lambda fucntion = function written in 1 line using lambda keyword
+#                   accepts any number of arguments, but only has one expression
+
+#lambda parameters: expression
+
+# def double(x):
+#     return x * 2
+
+# print(double(5))
+
+double = lambda x: x * 2
+print(double(5))
